88e12669fc72e90d2fa9bd46c611746f9d808dc0
Bernard <gicehajunior76@gmail.com>
2020-05-27 18:25:19 +0300

    local dataset preprocessing

--- CNN_Colored_Images.py
+++ CNN_Colored_Images.py
@@ -3,38 +3,59 @@ import matplotlib.pyplot as plot
 import model_train 
 import logging
 import os
-
-# global declaration of batch size
+import cv2
+global declaration of batch size
 BATCH_SIZE = ""
 image_shape = ""
 
+train_directory = 'Resources/microsoft_datasets/cats_and_dogs/tests_cats_and_dogs'
+validation_directory = 'Resources/microsoft_datasets/cats_and_dogs/train_cats_and_dogs'
+categories = ["cats", "dogs"]
+
 model_train.logging()
 
-def load_images_from_folder(train_cat_folder, train_dog_folder, test_cat_folder, test_dog_folder):
-    # return images
-    # train data
-    train_cat_data_dir = os.listdir(train_cat_folder)
-    train_dog_data_dir = os.listdir(train_dog_folder)
-    
-    # test data
-    test_cat_data_dir = os.listdir(test_cat_folder)
-    test_dog_data_dir = os.listdir(test_dog_folder)
-    
-    # dataset total per folder
-    train_total_data = len(train_cat_data_dir) + len(train_dog_data_dir)
-    test_total_data =len(test_cat_data_dir) + len(test_dog_data_dir) 
-    total_dataset = train_total_data + test_total_data  
-    
-    dataset = [train_cat_data_dir, train_dog_data_dir, test_cat_data_dir, test_dog_data_dir, train_total_data, test_total_data]
-    
-    return dataset
+def load_images(train_directory, test_directory, data_categories):
+    # preprocessing train data=> of cats and dogs
+    for category in data_categories:
+        train_data_path = os.path.join(train_directory, category)
+        test_data_path = os.path.join(test_directory, category)
+        
+        for train_image in os.listdir(train_data_path):
+            # reading the train data for each image
+            #and load them in Grayscale format
+            
+            train_image_array = cv2.imread(os.path.join(train_data_path, train_image))
+            
+            # you can shjow the image like 
+            # plot.imshow(train_image_array, cmap="gray")
+            # print(train_image_array)
+            # plot.show()
+            
+        for test_image in os.listdir(test_data_path):
+            # reading the train data for each image
+            #and load them in Grayscale format
+            
+            test_image_array = cv2.imread(os.path.join(test_data_path, test_image))
+            
+            # you can show the image like 
+            
+            # plot.imshow(test_image_array, cmap="gray")
+            # plot.show()
+            
+        # dataset total per folder
+        train_total_data = len(os.listdir(train_data_path))
+        test_total_data =len(os.listdir(test_data_path))
+        total_dataset = train_total_data + test_total_data  
+        
+        dataset = [train_image_array, test_image_array, train_total_data, test_total_data, ]
+
+        return dataset
 
 
-dataset_directories = load_images_from_folder(
-    'Resources/microsoft_datasets/cats_and_dogs/train_cats_and_dogs/cats', 
-    'Resources/microsoft_datasets/cats_and_dogs/train_cats_and_dogs/dogs',
-    'Resources/microsoft_datasets/cats_and_dogs/tests_cats_and_dogs/cata',
-    'Resources/microsoft_datasets/cats_and_dogs/tests_cats_and_dogs/dogs'
+dataset_directories = load_images(
+    train_directory,
+    validation_directory,
+    categories
     )
 
 #Images must be formatted into appropriately pre-processed floating point tensors before being fed into the network. 
@@ -45,7 +66,7 @@ dataset_directories = load_images_from_folder(
     #Convert them into floating point tensors
     #Rescale the tensors from values between 0 and 255 to values between 0 and 1, as neural networks prefer to deal with small input values.
     #preprocess using ImageDataGenerator
-def format_pictures(image_dir1, image_dir2):
+def format_pictures(training_data, testing_data):
     train_image_generator = ImageDataGenerator(
         rescale=1./255,
         
@@ -77,7 +98,7 @@ def format_pictures(image_dir1, image_dir2):
     
     test_data_generator = test_image_generator.flow_from_directory(
         batch_size = BATCH_SIZE,
-        directory = image_dir1,
+        directory = testing_data,
         shuffle = False,
         target_size = (image_shape, image_shape),
         class_mode = 'binary'
@@ -85,13 +106,13 @@ def format_pictures(image_dir1, image_dir2):
     
     train_data_generator = train_image_generator.flow_from_directory(
         batch_size = BATCH_SIZE,
-        directory = image_dir2,
+        directory = training_data,
         shuffle = True,
         target_size = (image_shape, image_shape),
         class_mode = 'binary'
         )
     
-    dataset = [test_data_generator, train_data_generator]
+    dataset = [train_data_generator, test_data_generator]
     
     return dataset
 
@@ -100,7 +121,7 @@ formated_images = format_pictures(dataset_directories[0], dataset_directories[1]
 test_dir_gen = formated_images[0]
 train_dir_gen = formated_images[1]
 
-# CREATE THE MODEL NOW
+# # CREATE THE MODEL NOW
 def model():
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation=tf.nn.relu input_shape=(150,150,3))
@@ -130,17 +151,15 @@ def optimize(unoptimized_model):
 optimized_model = optimize(model)
 
 # we can now train our model
-
-def train_model(model, train_data_dir, test_data_dir):
+def train_model(model, train_total_data, test_total_data, train_data_dir, test_data_dir):
     # Since our batches are coming from a generator(ImageDataGenerator), we'll use fit_generator instead of fit.
     # call the data and train our model
-    model = model.fit_generator(train_data_dir, epochs=100, steps_per_epoch=int(np.ceil(dataset_directories[4] / float(BATCH_SIZE))))
+    model = model.fit_generator(train_data_dir, epochs=100, steps_per_epoch=int(np.ceil(train_total_data / float(BATCH_SIZE))))
     
     
-    # model = model.fit_generator(test_data_dir, epochs=100, steps_per_epoch=int(np.ceil(dataset_directories[5] / float(BATCH_SIZE))))
+    model = model.fit_generator(test_data_dir, epochs=100, steps_per_epoch=int(np.ceil(test_total_data / float(BATCH_SIZE))))
     return model
 
-train_model(optimized_model, train_dir_gen, test_dir_gen)
+train_model(optimized_model, dataset_directories[3], dataset_directories[4], train_dir_gen, test_dir_gen)
     
-
-# The model now ready for predictions
+# # The model now ready for predictions
